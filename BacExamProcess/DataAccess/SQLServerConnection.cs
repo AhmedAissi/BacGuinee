62f5cd2af2d981e2a1f8dfb9f3f272cd52b91366
AhmedAissiHexaTribe <ahmedelaissi@hexatribe.com>
2021-06-12 14:06:50 +0100

    INTERFACE QR CODE SCAN

---
+++ BacExamProcess/DataAccess/SQLServerConnection.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BacExamProcess.DataAccess
+{
+    class SQLServerConnection
+    {
+        public static string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["conString"].ToString();
+        }
+        public static string sqlString;
+        public static SqlConnection connection = new SqlConnection();
+        public static SqlCommand SqlCommand = new SqlCommand("", connection);
+        public static SqlDataReader dataReader;
+        public static DataTable dataTable;
+        public static SqlDataAdapter dataAdapter;
+        public static void openConnection()
+        {
+            try
+            {
+                if (connection.State == ConnectionState.Closed)
+                {
+                    connection.ConnectionString = GetConnectionString();
+                    connection.Open();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Une erreur de connexion à la base de données est survenue"
+                    , "Veuillez contacter votre administrateur"
+                    , MessageBoxButton.OK
+                    , MessageBoxImage.Error);
+            }
+        }
+        public static void CloseConnection()
+        {
+            try
+            {
+                if (connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Une erreur de connexion à la base de données est survenue"
+                    , "Veuillez contacter votre administrateur"
+                    , MessageBoxButton.OK
+                    , MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ BacExamProcess/Windows/CentreExamen/WelcomePage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BacExamProcess
+{
+    /// <summary>
+    /// Logique d'interaction pour WelcomePage.xaml
+    /// </summary>
+    public partial class WelcomePage : Window
+    {
+        public WelcomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            DataEnveloppe win = new DataEnveloppe();
+            win.Show();
+        }
+    }
+}
